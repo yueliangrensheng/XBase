3ebd1fb23dc57c962c7801d60fab32c21c069f24
zhaishaoping <zhaishaoping@yuelvhui.com>
2020-08-27 17:47:33 +0800

    xbase update

--- XBase/src/main/java/com/yazao/lib/xbase/WBaseActivity.java
+++ XBase/src/main/java/com/yazao/lib/xbase/WBaseActivity.java
@@ -99,11 +99,14 @@ public abstract class WBaseActivity extends AppCompatActivity {
      */
     private void transparentStatusBar() {
         getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(Color.TRANSPARENT);
+            getWindow().setNavigationBarColor(Color.TRANSPARENT);
         }
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
